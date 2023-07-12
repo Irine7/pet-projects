51450886d578592d85e0bd90dbe60a67a08a5730
Irine7 <irine7@yandex.ru>
2023-07-12 22:54:01 +0200

    fix: likes function on new genetated posts

--- instagram-clone_ts/app.js
+++ instagram-clone_ts/app.js
@@ -1,19 +1,26 @@
 // Likes
 {
-	const like = document.querySelectorAll('.post__action:first-child');
+const postsBlock = document.querySelector('.posts');
 
-like.forEach((item, index) => {
-	item.addEventListener('click', () => {
-		if (item.classList.contains('active')) {
-			item.classList.remove('active');
-			item.innerHTML = '<i class="insta_like"></i>';
+// Event delegation to a parent node with a class '.post'
+postsBlock.addEventListener('click', (event) => {
+	// Check if the clicked item was a heart one
+	const target = event.target;
+	const isLikeClicked = target.classList.contains('insta_like') || target.classList.contains('insta_like_fill');
+
+	// Put/remove 'like' if the clicked item was a heart one
+	if (isLikeClicked) {
+		const parent = target.parentNode;
+		if (parent.classList.contains('active')) {
+			parent.classList.remove('active');
+			parent.innerHTML = '<i class="insta_like"></i>';
 		} else {
-			item.classList.add('active');
-			item.innerHTML = '<i class="insta_like_fill"></i>';
+			parent.classList.add('active');
+			parent.innerHTML = '<i class="insta_like_fill"></i>';
 		}
-	});
-});
-}
+	}
+})
+};
 
 // Posts render
 const defaultPostData = {
